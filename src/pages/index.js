248c75019480c3025429cc7fccded20e4dcba88b
Femi Andrew Akin <feolu2021@gmail.com>
2024-06-11 12:09:17 +0100

    Updated App.js
    Commented out unused algorithm

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import Acomplishments from '../components/Acomplishments/Acomplishments';
+/*import Acomplishments from '../components/Acomplishments/Acomplishments';
 import BgAnimation from '../components/BackgrooundAnimation/BackgroundAnimation';
 import Hero from '../components/Hero/Hero';
 import Projects from '../components/Projects/Projects';
@@ -23,3 +23,4 @@ const Home = () => {
 };
 
 export default Home;
+*/
